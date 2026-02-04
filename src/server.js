af71f13de26aee2a138a5ed292a0403d2715c231
Marcos Valladares <markosvalla@hotmail.com>
2026-02-03 21:04:06 -0300

    creamos el servidor y conectamos a base de datos

--- src/server.js
+++ src/server.js
@@ -1,2 +1,26 @@
 const express = require("express");
-require
+require("dotenv").config();
+const morgan = require("morgan");
+const path = require('path');
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+//IMPORTAMOS 
+const connectDB = require("./config/database");
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: true}))
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}}`)
+}) 
