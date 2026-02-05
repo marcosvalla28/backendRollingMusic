d7380f1924a622eb794f99cc7279d56f72a35da6
Marcos Valladares <markosvalla@hotmail.com>
2026-02-05 20:30:28 -0300

    creando modelos para usuario y canciones

---
+++ src/models/Song.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+
+const songSchema = new mongoose.Schema({
+
+    title:{
+        type: String,
+        required: true
+    },
+    artist:{
+        type: String,
+        required: true
+    },
+    ID:{
+        type: String,
+        unique: true,
+        default: () => crypto.randomUUID()
+    },
+    genre: {
+        type: String,
+        required: true,
+        enum: ['rock','pop', 'cumbia', 'bachata', 'trap', 'hip-hop', 'baladas', 'otro'],
+        unique: true
+    },
+    frontPage: {
+        type: String,
+        default: "https://rolling-music.vercel.app/assets/Logo-CK57mjsi.png",
+    },
+    duration: {
+        type: Number,
+        require: true,
+    },
+    audio: {
+        type: String,
+        require: true
+    }
+
+
+},{
+    timestamps: true
+});
+
+
+module.exports = mongoose.model('Song', songSchema);
