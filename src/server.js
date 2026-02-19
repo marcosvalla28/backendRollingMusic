70664943fa1fa218a64cd513d949f6ff488b28d6
Juan <juanmd149@gmail.com>
2026-02-19 11:04:20 -0300

    routesserver

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-//IMPORTAMOS 
 const authRoutes = require("./routes/auth.routes");
 const songRoutes = require("./routes/song.routes");
 const userRoutes = require("./routes/user.routes");
+const playlistRoutes = require("./routes/playlist.routes"); 
 const connectDB = require("./config/database");
 const createSuperAdmin = require("./utils/createSuperAdmin");
 const errorHanlder = require("./middlewares/errorHandler");
@@ -17,12 +17,11 @@ const favoritesRoutes = require("./routes/favorite.routes");
 const app = express();
 
 connectDB();
-
 createSuperAdmin();
 
 app.use(cors({
   origin: "https://rolling-music.vercel.app",
-  credentials: true  //recibir cookie 
+  credentials: true
 }));
 app.use(morgan("dev"));
 app.use(express.json());
@@ -30,31 +29,24 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
 
 app.use('/uploads', express.static(path.join(__dirname, '../uploads'), {
-    setHeaders: (res, path) => {
-        // 🛠️ 1. Permitir que el frontend (Vite) lea el archivo
-        res.set('Access-Control-Allow-Origin', 'http://localhost:5173');
-        
-        // 🛠️ 2. FORZAR REPRODUCCIÓN (Esto mata la descarga)
-        // 'inline' le dice al navegador que lo abra en el reproductor interno
+    setHeaders: (res, filePath) => {
+        res.set('Access-Control-Allow-Origin', '*');
         res.set('Content-Disposition', 'inline');
-        
-        // 🛠️ 3. Asegurar el tipo de archivo para audios
-        if (path.endsWith('.mp3')) {
+        if (filePath.endsWith('.mp3')) {
             res.set('Content-Type', 'audio/mpeg');
         }
     }
 }));
 
-//ENRUTADORES
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/song", songRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/favorites", favoritesRoutes);
+app.use("/api/v1/playlists", playlistRoutes);
 
-//ACA HAY QUE LLAMAR AL MIDDELWARE MANEJADOR DE ERRORES
 app.use(errorHanlder);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`)
-})
+});
